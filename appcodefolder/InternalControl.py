ed29ce5d65c49c3e224a24dd2695b7464dbd7ca9
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-09 22:33:13 -0600

    ...

--- appcodefolder/InternalControl.py
+++ appcodefolder/InternalControl.py
@@ -1,10 +1,9 @@
 import os
 
 class cInternalControl:
-    idControl=9
     timeout=70
     hfolder='appcodefolder' 
-    heroku=False
+    heroku=True
     rutaHeroku='/app/'+hfolder
     rutaLocal=os.getcwd()+'\\'+hfolder+'\\'
     download_dir='Download_cjf_by_folder'
